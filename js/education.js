8ac736cc02b9828641deee3a8e513fac79379d5f
Kunlakan (Jeen) Cherdchusilp <kunlakan@uw.edu>
2017-02-15 21:15:23 -0800

    Update education / add bootstrap

--- js/education.js
+++ js/education.js
@@ -3,12 +3,12 @@ jQuery.getJSON("js/data.json", function(data){
      var school = data["school"];
      
      for(var i = 0; i < school.length; i++) {
-          str += "<div class='left'><a target='_blank' href='" + school[i].site + "'>";
-          str += "<img src='" + school[i].logo +"' class='logo'></a></div>";
-          str += "<div class='center'><h3>" + school[i].name + "<br>";
-          str += school[i].degree + "</h3></div>";
+          str += "<div class='col-xs-12><a target='_blank' href='" + school[i].site + "'>";
+          str += "<img src='" + school[i].logo +"' class='logo'></a>";
           str += "<div class='right'>" + school[i].graduation + "<br>";
-          str += "" + school[i].area + "</div>";
+          str += school[i].area + "</div>";
+          str += "<div class='col-xs-12 center'><h4>" + school[i].name + "<br>";
+          str += school[i].degree + "</h4></div></div>";
                     
           if(school[i].course.length > 0){
                str += "<div class='full'>Related Coursework:<ul>";
@@ -19,7 +19,7 @@ jQuery.getJSON("js/data.json", function(data){
           }
          
           if(school[i].honor.length > 0){
-               str += "<div class='full'>Honors & Awards:<ul>";
+               str += "<div class='col-xs-12 full'>Honors & Awards:<ul>";
                school[i].honor.forEach(function(honorname){
                     str += "<li>" + honorname + "</li>";
                });
